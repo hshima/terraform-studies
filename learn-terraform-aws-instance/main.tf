698a4d1514d5f5beb1d48dbd109fbf0f5f45b1b7
hshima <22624182+hshima@users.noreply.github.com>
2023-08-09 23:29:39 -0300

    Primeira configuração de terraform para EC2 em AMI de amazon linux

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,24 @@
+# Build infrastructure
+# https://developer.hashicorp.com/terraform/tutorials/aws-get-started/aws-build
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09538990a0c4fe9be"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
